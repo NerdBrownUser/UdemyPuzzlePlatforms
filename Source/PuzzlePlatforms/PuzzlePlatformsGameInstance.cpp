02a3829e0101eba096933bd31eaa619824526eb0
NerdBrownUser <kangjoon90@naver.com>
2023-02-28 22:03:17 +0900

    Add GameInstance

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	Super::Init();
+}
