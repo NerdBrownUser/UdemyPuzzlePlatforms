4c62fe960e4e06af046d9b6d7ef66f9fc8491ffa
NerdBrownUser <kangjoon90@naver.com>
2023-03-01 17:29:40 +0900

    Update Join command

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -34,5 +34,12 @@ void UPuzzlePlatformsGameInstance::Join(const FString& ipAddress)
 	if (GEngine != nullptr)
 	{
 		GEngine->AddOnScreenDebugMessage(0, 2.0f, FColor::Green, FString::Printf(_T("Joining %s"), *ipAddress));
+	
+		auto* playerController = GetFirstLocalPlayerController();
+
+		if (playerController != nullptr)
+		{
+			playerController->ClientTravel(ipAddress, ETravelType::TRAVEL_Absolute);
+		}
 	}
 }
